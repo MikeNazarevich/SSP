7d1b30612a61a99d87e115f9c9b6960a50ca9980
mishka3097 <mishka300397@gmail.com>
2017-12-03 03:11:09 +0300

    Create 4a,b

---
+++ lab5/lab-5/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Icon from './Icon/Icon';
+import IconPlusText from './IconPlusText/IconPlusText';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="App-intro">
+
+
+          {/* <Icon icon="address-card-o"/>
+          <Icon icon="map-marker"/>
+          <Icon icon="envelope-o"/>
+          <Icon icon="link"/> */}
+          <IconPlusText />
+
+          
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lab5/lab-5/src/IconPlusText/IconPlusText.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+// import './IconPlusText.css';
+import Icon from '../Icon/Icon';
+import Text from '../Text';
+
+class IconPlusText extends Component {
+  render() {
+    return (
+      <div className="IconPlusText">
+        <Icon icon="address-card-o"/>
+        <Text text="@facebook"/>
+
+        <Icon icon="map-marker"/>
+        <Text text="London, UK"/>
+
+        <Icon icon="envelope-o"/>
+        <Text href="kudato.com" text="Dan Abraham"/>
+
+        <Icon icon="link"/>
+        <Text href="tudato.com" text="twitter.com"/>
+      </div>
+    );
+  }
+}
+
+export default IconPlusText;
